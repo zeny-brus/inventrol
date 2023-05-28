30b5ccf1816eb858ff90a7e2d06c766cad8ae9cd
Gustavo do Carmo <gustavosoares.nh@gmail.com>
2023-05-28 19:20:56 -0300

    aplicando regra de formulario de movimentos

--- inventrol/core/forms.py
+++ inventrol/core/forms.py
@@ -20,10 +20,20 @@ class MovementForm(forms.ModelForm):
         ('OUTPUT','Output'),
     ]
     type_movement = forms.ChoiceField(choices=TYPE_MOVEMENT, widget=forms.RadioSelect)
+    
     class Meta:
         model = Movement
         fields = ("product","type_movement","amout")
     
+    def clean(self):
+        cleaned_data = super().clean()
+        type_movement = cleaned_data.get('type_movement')
+        amout = cleaned_data.get('amout')
+        product = cleaned_data.get('product')
+
+        if type_movement == 'OUTPUT' and amout > product.amout:
+            self.add_error('amout','A quantidade de saída é maior que a quantidade em estoque')
+        return cleaned_data
 
 
 
